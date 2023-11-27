dd026fa3b11d2406cc77429176ce85a301a78e8b
thái giản đình <giandinhthai@gmail.com>
2023-11-27 17:20:33 +0700

    add database

--- homefix.sql
+++ homefix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 09:37 AM
+-- Generation Time: Nov 27, 2023 at 11:19 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,13 +31,11 @@ USE `homefix`;
 -- Creation: Nov 27, 2023 at 08:32 AM
 --
 
-CREATE TABLE IF NOT EXISTS `agreement` (
+CREATE TABLE `agreement` (
   `agreement_id` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `time_schedule` datetime NOT NULL,
-  `order_id` int(11) NOT NULL,
-  PRIMARY KEY (`agreement_id`),
-  KEY `order_id` (`order_id`)
+  `order_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -54,13 +52,11 @@ CREATE TABLE IF NOT EXISTS `agreement` (
 -- Creation: Nov 27, 2023 at 08:32 AM
 --
 
-CREATE TABLE IF NOT EXISTS `feedback` (
+CREATE TABLE `feedback` (
   `feedback_id` int(11) NOT NULL,
   `rate` smallint(6) NOT NULL,
   `comment` text NOT NULL,
-  `order_id` int(11) NOT NULL,
-  PRIMARY KEY (`feedback_id`),
-  KEY `order_id` (`order_id`)
+  `order_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -77,10 +73,9 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 -- Creation: Nov 27, 2023 at 08:30 AM
 --
 
-CREATE TABLE IF NOT EXISTS `make` (
+CREATE TABLE `make` (
   `order_id` int(11) NOT NULL,
-  `agreement_id` int(11) NOT NULL,
-  PRIMARY KEY (`order_id`,`agreement_id`)
+  `agreement_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -99,10 +94,9 @@ CREATE TABLE IF NOT EXISTS `make` (
 -- Creation: Nov 27, 2023 at 08:31 AM
 --
 
-CREATE TABLE IF NOT EXISTS `receive` (
+CREATE TABLE `receive` (
   `order_id` int(11) NOT NULL,
-  `feedback_id` int(11) NOT NULL,
-  PRIMARY KEY (`order_id`,`feedback_id`)
+  `feedback_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -121,10 +115,9 @@ CREATE TABLE IF NOT EXISTS `receive` (
 -- Creation: Nov 27, 2023 at 04:22 AM
 --
 
-CREATE TABLE IF NOT EXISTS `repair_type` (
+CREATE TABLE `repair_type` (
   `provider_id` int(11) NOT NULL,
-  `repair_type` enum('"Nội thất"','"Đồ gia dụng"','"Dụng cụ nhà bếp"','"Vật dụng công nghệ"') NOT NULL,
-  PRIMARY KEY (`provider_id`,`repair_type`)
+  `repair_type` enum('"Nội thất"','"Đồ gia dụng"','"Dụng cụ nhà bếp"','"Vật dụng công nghệ"') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -138,10 +131,10 @@ CREATE TABLE IF NOT EXISTS `repair_type` (
 --
 -- Table structure for table `service order`
 --
--- Creation: Nov 27, 2023 at 03:53 AM
+-- Creation: Nov 27, 2023 at 09:05 AM
 --
 
-CREATE TABLE IF NOT EXISTS `service order` (
+CREATE TABLE `service order` (
   `order_id` int(11) NOT NULL,
   `item_type` enum('"Nội thất"','"Đồ gia dụng"','"Dụng cụ nhà bếp"','"Vật dụng công nghệ"') NOT NULL,
   `specific_item` varchar(40) NOT NULL,
@@ -151,13 +144,10 @@ CREATE TABLE IF NOT EXISTS `service order` (
   `district` varchar(40) NOT NULL,
   `town` varchar(40) NOT NULL,
   `street` varchar(50) NOT NULL,
-  `time_range` datetime NOT NULL DEFAULT current_timestamp(),
+  `time_range` datetime NOT NULL,
   `status` enum('"Đang xác nhận"','"Đã hủy"','"Đã hoàn thành"','"Đang chờ thực hiện"','"Xác thực hoàn tất"') NOT NULL,
   `customer_id` int(11) NOT NULL,
-  `provider_id` int(11) NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `customer_id` (`customer_id`),
-  KEY `provider_id` (`provider_id`)
+  `provider_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -173,47 +163,111 @@ CREATE TABLE IF NOT EXISTS `service order` (
 --
 -- Table structure for table `user_customer`
 --
--- Creation: Nov 27, 2023 at 08:19 AM
+-- Creation: Nov 27, 2023 at 10:14 AM
+-- Last update: Nov 27, 2023 at 10:15 AM
 --
 
-CREATE TABLE IF NOT EXISTS `user_customer` (
+CREATE TABLE `user_customer` (
   `customer_id` int(11) NOT NULL,
   `user_name` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `name` varchar(40) NOT NULL,
-  `balance` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`customer_id`),
-  UNIQUE KEY `user_name` (`user_name`)
+  `balance` decimal(10,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `user_customer`:
 --
 
+--
+-- Dumping data for table `user_customer`
+--
+
+INSERT INTO `user_customer` (`customer_id`, `user_name`, `password`, `name`, `balance`) VALUES
+(0, 'customer1', '$2b$10$LmHWQ8e/4zG1lhc6LJtVZOYEzRPw86fgOpuj7/GBJFwNAQ4AXOQkK', '', 0.00);
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user_provider`
 --
--- Creation: Nov 27, 2023 at 04:14 AM
+-- Creation: Nov 27, 2023 at 10:14 AM
+-- Last update: Nov 27, 2023 at 10:14 AM
 --
 
-CREATE TABLE IF NOT EXISTS `user_provider` (
+CREATE TABLE `user_provider` (
   `provider_id` int(11) NOT NULL,
   `user_name` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `name` int(40) NOT NULL,
-  `balance` decimal(10,2) NOT NULL,
-  `province` int(20) NOT NULL,
-  `town` int(20) NOT NULL,
-  `street` int(40) NOT NULL,
-  PRIMARY KEY (`provider_id`),
-  UNIQUE KEY `user_name` (`user_name`)
+  `password` varchar(100) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `balance` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `province` varchar(20) NOT NULL,
+  `district` varchar(20) NOT NULL,
+  `town` varchar(20) NOT NULL,
+  `street` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `user_provider`:
 --
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agreement`
+--
+ALTER TABLE `agreement`
+  ADD PRIMARY KEY (`agreement_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `make`
+--
+ALTER TABLE `make`
+  ADD PRIMARY KEY (`order_id`,`agreement_id`);
+
+--
+-- Indexes for table `receive`
+--
+ALTER TABLE `receive`
+  ADD PRIMARY KEY (`order_id`,`feedback_id`);
+
+--
+-- Indexes for table `repair_type`
+--
+ALTER TABLE `repair_type`
+  ADD PRIMARY KEY (`provider_id`,`repair_type`);
+
+--
+-- Indexes for table `service order`
+--
+ALTER TABLE `service order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `provider_id` (`provider_id`);
+
+--
+-- Indexes for table `user_customer`
+--
+ALTER TABLE `user_customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- Indexes for table `user_provider`
+--
+ALTER TABLE `user_provider`
+  ADD PRIMARY KEY (`provider_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
 
 --
 -- Constraints for dumped tables
@@ -257,6 +311,48 @@ ALTER TABLE `repair_type`
 ALTER TABLE `service order`
   ADD CONSTRAINT `service order_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `user_customer` (`customer_id`),
   ADD CONSTRAINT `service order_ibfk_2` FOREIGN KEY (`provider_id`) REFERENCES `user_provider` (`provider_id`);
+
+
+--
+-- Metadata
+--
+USE `phpmyadmin`;
+
+--
+-- Metadata for table agreement
+--
+
+--
+-- Metadata for table feedback
+--
+
+--
+-- Metadata for table make
+--
+
+--
+-- Metadata for table receive
+--
+
+--
+-- Metadata for table repair_type
+--
+
+--
+-- Metadata for table service order
+--
+
+--
+-- Metadata for table user_customer
+--
+
+--
+-- Metadata for table user_provider
+--
+
+--
+-- Metadata for database homefix
+--
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
