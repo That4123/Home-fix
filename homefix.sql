bfb7a11f232570944fd3677d9ef31d82d615ab80
giandinhthai <95992476+giandinhthai@users.noreply.github.com>
2024-01-01 10:32:42 +0700

    Update homefix.sql

--- homefix.sql
+++ homefix.sql
@@ -3,10 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2024 at 03:57 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
+-- Generation Time: Dec 02, 2023 at 03:26 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+DROP database IF EXISTS homefix;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -20,149 +20,130 @@ SET time_zone = "+00:00";
 --
 -- Database: `homefix`
 --
+CREATE DATABASE IF NOT EXISTS `homefix` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `homefix`;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `agreement`
 --
+-- Creation: Dec 02, 2023 at 06:29 AM
+--
 
-CREATE TABLE `agreement` (
-  `agreement_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `agreement` (
+  `agreement_id` int(11) NOT NULL AUTO_INCREMENT,
   `price` decimal(10,2) NOT NULL,
   `time_schedule` datetime NOT NULL,
-  `order_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `completedorder`
---
-
-CREATE TABLE `completedorder` (
   `order_id` int(11) NOT NULL,
-  `description` text DEFAULT NULL,
-  `order_status` enum('total','partial','not') DEFAULT NULL,
-  `wage` int(11) DEFAULT NULL
+  PRIMARY KEY (`agreement_id`),
+  KEY `order_id` (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `completedorder`
+-- RELATIONSHIPS FOR TABLE `agreement`:
+--   `order_id`
+--       `service_order` -> `order_id`
 --
-
-INSERT INTO `completedorder` (`order_id`, `description`, `order_status`, `wage`) VALUES
-(1, 'Máy bị cháy cuộn dây', 'total', 100000),
-(2, 'Lỗi nguồn điện, đã tiến hành đo áp và thay thế', 'total', 100000),
-(3, 'Thiết bị lỗi phần điện tử, không phải phần cơ như mô tả. Không thể sửa chữa', 'not', 100000),
-(4, 'Bị mất linh kiện, đã bổ sung nhưng máy chạy vẫn bị rung lắc', 'partial', 100000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `completedorderpic`
---
-
-CREATE TABLE `completedorderpic` (
-  `pic_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `image` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `completedorderpic`
---
-
-INSERT INTO `completedorderpic` (`pic_id`, `order_id`, `image`) VALUES
-(1, 1, 'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg'),
-(2, 1, 'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg'),
-(3, 1, 'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg'),
-(4, 1, 'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `completedorderpricelist`
---
-
-CREATE TABLE `completedorderpricelist` (
-  `component_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `component_name` varchar(50) DEFAULT NULL,
-  `cost` int(11) DEFAULT NULL,
-  `description` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `completedorderpricelist`
---
-
-INSERT INTO `completedorderpricelist` (`component_id`, `order_id`, `component_name`, `cost`, `description`) VALUES
-(1, 1, 'Bộ ốc vít', 50000, 'thay mới'),
-(2, 1, 'Lõi đồng', 35000, 'bị cháy, thay tạm lõi cũ'),
-(3, 1, 'Phích cắm', 5000, 'thay mới');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `feedback`
 --
+-- Creation: Dec 02, 2023 at 06:29 AM
+--
 
-CREATE TABLE `feedback` (
-  `feedback_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `feedback_id` int(11) NOT NULL AUTO_INCREMENT,
   `rate` smallint(6) NOT NULL,
   `comment` text NOT NULL,
-  `order_id` int(11) NOT NULL
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`feedback_id`),
+  KEY `order_id` (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `feedback`
+-- RELATIONSHIPS FOR TABLE `feedback`:
+--   `order_id`
+--       `service_order` -> `order_id`
 --
-
-INSERT INTO `feedback` (`feedback_id`, `rate`, `comment`, `order_id`) VALUES
-(3, 4, 'Dat', 1);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `make`
 --
+-- Creation: Dec 02, 2023 at 06:32 AM
+--
 
-CREATE TABLE `make` (
+CREATE TABLE IF NOT EXISTS `make` (
   `order_id` int(11) NOT NULL,
-  `agreement_id` int(11) NOT NULL
+  `agreement_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`,`agreement_id`),
+  KEY `agreement_id` (`agreement_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `make`:
+--   `order_id`
+--       `service_order` -> `order_id`
+--   `agreement_id`
+--       `agreement` -> `agreement_id`
+--
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `receive`
 --
+-- Creation: Dec 02, 2023 at 06:29 AM
+--
 
-CREATE TABLE `receive` (
+CREATE TABLE IF NOT EXISTS `receive` (
   `order_id` int(11) NOT NULL,
-  `feedback_id` int(11) NOT NULL
+  `feedback_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`,`feedback_id`),
+  KEY `feedback_id` (`feedback_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `receive`:
+--   `feedback_id`
+--       `feedback` -> `feedback_id`
+--   `order_id`
+--       `service_order` -> `order_id`
+--
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `repair_type`
 --
+-- Creation: Dec 02, 2023 at 01:36 PM
+--
 
-CREATE TABLE `repair_type` (
+CREATE TABLE IF NOT EXISTS `repair_type` (
   `provider_id` int(11) NOT NULL,
-  `repair_type` enum('Nội thất','Đồ gia dụng','Dụng cụ nhà bếp','Vật dụng công nghệ') NOT NULL
+  `repair_type` enum('Nội thất','Đồ gia dụng','Dụng cụ nhà bếp','Vật dụng công nghệ') NOT NULL,
+  PRIMARY KEY (`provider_id`,`repair_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `repair_type`:
+--
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `service_order`
 --
+-- Creation: Dec 02, 2023 at 06:29 AM
+-- Last update: Dec 02, 2023 at 02:24 PM
+--
 
-CREATE TABLE `service_order` (
-  `order_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `service_order` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_type` enum('Nội thất','Đồ gia dụng','Dụng cụ nhà bếp','Vật dụng công nghệ') NOT NULL,
   `specific_item` varchar(40) NOT NULL,
   `text_description` text NOT NULL,
@@ -175,52 +156,73 @@ CREATE TABLE `service_order` (
   `status` enum('Đang xác nhận','Đã hủy','Đã hoàn thành','Đang chờ thực hiện','Xác thực hoàn tất') NOT NULL,
   `customer_id` int(11) NOT NULL,
   `provider_id` int(11) NOT NULL,
-  `paid` int(11) DEFAULT 0,
-  `fixed` varchar(50) DEFAULT NULL,
-  `fixed_des` text DEFAULT NULL,
-  `wage` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`order_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `provider_id` (`provider_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `service_order`:
+--   `customer_id`
+--       `user_customer` -> `customer_id`
+--   `provider_id`
+--       `user_provider` -> `provider_id`
+--
 
 --
 -- Dumping data for table `service_order`
 --
 
-INSERT INTO `service_order` (`order_id`, `item_type`, `specific_item`, `text_description`, `image_description`, `province`, `district`, `town`, `street`, `time_range`, `status`, `customer_id`, `provider_id`, `paid`, `fixed`, `fixed_des`, `wage`) VALUES
-(1, 'Vật dụng công nghệ', 'abc', 'abcy', 'abcx', 'phu tho', 'ba dinh', 'kontum', 'street', '2023-12-02 20:33:25', 'Đã hủy', 1, 2, 0, 'total', NULL, 0),
-(2, 'Vật dụng công nghệ', 'abc', 'abcy', 'abcx', 'phu tho', 'ba dinh', 'kontum', 'street', '2023-12-02 21:09:33', 'Đã hủy', 1, 2, 0, 'total', NULL, 0),
-(3, 'Đồ gia dụng', 'Máy giặt Toshiba 15x', 'Không vào điện', 'https://tse1.mm.bing.net/th?id=OIP.h43m6', 'Tỉnh Vĩnh Phúc', 'Huyện Tam Đảo', 'Thị trấn Đại Đình', '220, Hùng Vương', '2023-12-02 21:08:00', 'Đang xác nhận', 1, 2, 0, 'partial', NULL, 0),
-(4, 'Đồ gia dụng', 'Máy giặt Toshiba 15x', 'Không vào điện', 'https://tse1.mm.bing.net/th?id=OIP.h43m6', 'Tỉnh Vĩnh Phúc', 'Huyện Tam Đảo', 'Thị trấn Đại Đình', 'tran phu', '2023-12-02 21:08:00', 'Đang xác nhận', 1, 2, 0, 'not', NULL, 0);
+INSERT INTO `service_order` (`order_id`, `item_type`, `specific_item`, `text_description`, `image_description`, `province`, `district`, `town`, `street`, `time_range`, `status`, `customer_id`, `provider_id`) VALUES
+(1, 'Vật dụng công nghệ', 'Máy tính HP', 'Tắt màn hình', 'https://cdn.tgdd.vn/Files/2021/09/05/1380257/h2_1280x720-800-resize.jpg', 'Tỉnh Phú Thọ', 'Ba Đình', 'Phường Bình An', '267 Nguyễn Thái Bình', '2023-12-02 20:33:25', 'Đã huỷ', 1, 2),
+(2, 'Vật dụng công nghệ', 'Máy in Xerox', 'Hết mực in', 'https://vitinhnguyenkim.vn/uploads/product/may-in-may-scan/Fuji-Xerox-DocuPrint-3205.png', 'Tỉnh Phú Thọ', 'Ba Đình', 'Phường An Bình', '234 Quang Trung', '2023-12-03 21:09:33', 'Đã huỷ', 1, 2),
+(3, 'Đồ gia dụng', 'Máy giặt Toshiba 15x', 'Không vào điện', 'https://tse1.mm.bing.net/th?id=OIP.h43m6JfyuhSB1ZgT7-_h0QHaJ4&pid=Api&P=0&h=220', 'Tỉnh Vĩnh Phúc', 'Huyện Tam Đảo', 'Thị trấn Đại Đình', '220 Hùng Vương', '2023-12-04 21:08:00', 'Đã hoàn thành', 1, 2),
+(4, 'Đồ gia dụng', 'Máy giặt Toshiba 15x', 'Không vào điện', 'https://tse1.mm.bing.net/th?id=OIP.h43m6JfyuhSB1ZgT7-_h0QHaJ4&pid=Api&P=0&h=220', 'Tỉnh Vĩnh Phúc', 'Huyện Tam Đảo', 'Thị trấn Đại Đình', '35 Trần Phú', '2023-12-05 21:08:00', 'Đang xác nhận', 1, 2),
+(5, 'Đồ gia dụng', 'Máy giặt Toshiba 15x', 'Không thể bơm nước', 'https://tse1.mm.bing.net/th?id=OIP.h43m6JfyuhSB1ZgT7-_h0QHaJ4&pid=Api&P=0&h=220', 'TP Hồ Chí Minh', 'Quận 5', 'Phường 11', '220 Trần Hưng Đạo', '2023-12-06 21:10:00', 'Đang xác nhận', 1, 2),
+(6, 'Đồ gia dụng', 'Máy giặt Toshiba 15x', 'Tiếng động mạnh khi hoạt động', 'https://tse1.mm.bing.net/th?id=OIP.h43m6JfyuhSB1ZgT7-_h0QHaJ4&pid=Api&P=0&h=220', 'TP Hồ Chí Minh', 'Quận 12', 'Phường 12', '234 Tô Hiến Thành', '2023-12-07 21:28:00', 'Đã hoàn thành', 1, 2);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user_customer`
 --
+-- Creation: Dec 02, 2023 at 02:00 PM
+-- Last update: Dec 02, 2023 at 02:00 PM
+--
 
-CREATE TABLE `user_customer` (
-  `customer_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `name` varchar(40) NOT NULL,
   `balance` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `phone_number` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `phone_number` varchar(10) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `user_customer`:
+--
 
 --
 -- Dumping data for table `user_customer`
 --
 
 INSERT INTO `user_customer` (`customer_id`, `user_name`, `password`, `name`, `balance`, `phone_number`) VALUES
-(1, 'customer1', '$2b$10$LmHWQ8e/4zG1lhc6LJtVZOYEzRPw86fgOpuj7/GBJFwNAQ4AXOQkK', 'Trần Thị A', 0.00, '');
+(1, 'customer1', '$2b$10$LmHWQ8e/4zG1lhc6LJtVZOYEzRPw86fgOpuj7/GBJFwNAQ4AXOQkK', 'Trần Thị An', 0.00, '');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user_provider`
 --
+-- Creation: Dec 02, 2023 at 01:59 PM
+-- Last update: Dec 02, 2023 at 01:59 PM
+--
 
-CREATE TABLE `user_provider` (
-  `provider_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_provider` (
+  `provider_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
   `name` varchar(40) NOT NULL,
@@ -230,128 +232,27 @@ CREATE TABLE `user_provider` (
   `town` varchar(20) NOT NULL,
   `street` varchar(40) NOT NULL,
   `phone_number` varchar(10) NOT NULL,
-  `rate` decimal(10,2) NOT NULL DEFAULT 0.00
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `rate` decimal(10,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`provider_id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELATIONSHIPS FOR TABLE `user_provider`:
+--
 
 --
 -- Dumping data for table `user_provider`
 --
 
 INSERT INTO `user_provider` (`provider_id`, `user_name`, `password`, `name`, `balance`, `province`, `district`, `town`, `street`, `phone_number`, `rate`) VALUES
-(2, 'provider1', '$2b$10$iZkUu9bm2731fYMEd75aGOY6jwpYUPomTtmBOa6zTj2AW8cD9FVlW', 'Nguyễn Văn A', 0.00, 'Hcm', 'q10', 'phú ', '112', '1234567890', 4.70);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `agreement`
---
-ALTER TABLE `agreement`
-  ADD PRIMARY KEY (`agreement_id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `completedorder`
---
-ALTER TABLE `completedorder`
-  ADD PRIMARY KEY (`order_id`);
-
---
--- Indexes for table `completedorderpic`
---
-ALTER TABLE `completedorderpic`
-  ADD PRIMARY KEY (`pic_id`,`order_id`);
-
---
--- Indexes for table `completedorderpricelist`
---
-ALTER TABLE `completedorderpricelist`
-  ADD PRIMARY KEY (`component_id`,`order_id`);
-
---
--- Indexes for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`feedback_id`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `make`
---
-ALTER TABLE `make`
-  ADD PRIMARY KEY (`order_id`,`agreement_id`),
-  ADD KEY `agreement_id` (`agreement_id`);
-
---
--- Indexes for table `receive`
---
-ALTER TABLE `receive`
-  ADD PRIMARY KEY (`order_id`,`feedback_id`),
-  ADD KEY `feedback_id` (`feedback_id`);
-
---
--- Indexes for table `repair_type`
---
-ALTER TABLE `repair_type`
-  ADD PRIMARY KEY (`provider_id`,`repair_type`);
-
---
--- Indexes for table `service_order`
---
-ALTER TABLE `service_order`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `provider_id` (`provider_id`);
-
---
--- Indexes for table `user_customer`
---
-ALTER TABLE `user_customer`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD UNIQUE KEY `user_name` (`user_name`);
-
---
--- Indexes for table `user_provider`
---
-ALTER TABLE `user_provider`
-  ADD PRIMARY KEY (`provider_id`),
-  ADD UNIQUE KEY `user_name` (`user_name`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `agreement`
---
-ALTER TABLE `agreement`
-  MODIFY `agreement_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `feedback`
---
-ALTER TABLE `feedback`
-  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `service_order`
---
-ALTER TABLE `service_order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `user_customer`
---
-ALTER TABLE `user_customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `user_provider`
---
-ALTER TABLE `user_provider`
-  MODIFY `provider_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
+(2, 'provider1', '$2b$10$iZkUu9bm2731fYMEd75aGOY6jwpYUPomTtmBOa6zTj2AW8cD9FVlW', 'Nguyễn Văn Anh', 0.00, 'TP Hồ Chí Minh', 'Q10', 'Phường 14 ', '35 Thành Thái', '0975667874', '4.7'),
+(3, 'provider2', '$2b$10$xk1fKCFjroW6.waXRqAwDuClWp6xLAXtWr1cOgNAND2lamE74fXA2', 'Nguyễn Hoàng Thành', 0.00, 'TP Hồ Chí Minh', 'Q5 ', 'Phường 11', '234 Trần Hưng Đạo', '0926484832', '4.9'),
+(4, 'provider3', '$2b$10$HHpxAcehWOO0GyXSUhgDPeJs25YiwfKuUCP34icqQey9CZTwN2/ZC', 'Phạm Hoàng An', 0.00, 'TP Hồ Chí Minh', 'Q1 ', 'Phường Tân Định','134 Trần Quang Khải', '0926484834', '4.5'),
+(5, 'provider4', '$2b$10$w/S73eXYIDD.cfhWUnJA4OgTB5gv9ocoGSzBdEDye1P6x/4jaz9x6', 'Vũ Trọng Phúc', 0.00, 'TP Hồ Chí Minh', 'Q10 ', 'Phường 12','254 Tô Hiến Thành', '0974584832', '4.6'),
+(6, 'provider5', '$2b$10$OJMCJUgo7wzJ.1HcFB2RLuPseSzAown7MAp09TFrzhwRwzMEs70fW', 'Phạm Thành', 0.00, 'TP Hồ Chí Minh', 'Q10 ', 'Phường 12','296 Tô Hiến Thành', '0974584856', '4.0'),
+(7, 'provider6', '$2b$10$/5VLno5RYfpQGveWGlZUFueIyE6dqb1ashP8N2i5uUY5GhxHSn/jq', 'Phan Lê Hoàng An', 0.00, 'TP Hồ Chí Minh', 'Thủ Đức ', 'Phường Bình Thọ','16 Võ Văn Ngân', '0974584832', '4.6'),
+(8, 'provider7', '$2b$10$JEYWbDehukLRZNO8.2OFZu2LUKue4KHBKCXOMqgQ/wRPNOGx7BPJW', 'Phùng Thanh', 0.00, 'Tỉnh Vĩnh Phúc', 'Huyện Tam Đảo ', 'Thị trấn Đại Đình','254 Tô Hiến Thành', '0974584556', '4.0');
 --
 -- Constraints for dumped tables
 --
@@ -388,6 +289,94 @@ ALTER TABLE `receive`
 ALTER TABLE `service_order`
   ADD CONSTRAINT `service_order_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `user_customer` (`customer_id`),
   ADD CONSTRAINT `service_order_ibfk_2` FOREIGN KEY (`provider_id`) REFERENCES `user_provider` (`provider_id`);
+
+DROP TABLE IF EXISTS CompletedOrder;
+CREATE TABLE CompletedOrder (
+	order_id INT,
+    description TEXT,
+    order_status ENUM ('total','partial','not'),
+    wage INT,
+    PRIMARY KEY (order_id)
+);
+
+DROP TABLE IF EXISTS CompletedOrderPic;
+CREATE TABLE CompletedOrderPic (
+	pic_id INT,
+    order_id INT,
+    image TEXT,
+    PRIMARY KEY (pic_id, order_id)
+);
+
+DROP TABLE IF EXISTS CompletedOrderPriceList;
+CREATE TABLE CompletedOrderPriceList (
+    component_id INT auto_increment,
+    order_id INT,
+    component_name VARCHAR(50),
+    cost INT,
+    description text,
+    PRIMARY KEY (component_id, order_id)
+);
+
+INSERT INTO CompletedOrder (order_id, description, order_status,wage)
+VALUES
+(1,'Máy bị cháy cuộn dây', 'total',100000),
+(2,'Lỗi nguồn điện, đã tiến hành đo áp và thay thế', 'total',100000),
+(3,'Thiết bị lỗi phần điện tử, không phải phần cơ như mô tả. Không thể sửa chữa','not',100000),
+(4,'Bị mất linh kiện, đã bổ sung nhưng máy chạy vẫn bị rung lắc','partial',100000);
+
+INSERT INTO CompletedOrderPic (pic_id, order_id, image)
+VALUES
+(1,1, 'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg'),
+(2,1, 'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg'),
+(3,1,'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg'),
+(4,1,'https://i.pinimg.com/736x/8d/85/02/8d850252f18eb6016ffae77c07e61438.jpg');
+
+INSERT INTO CompletedOrderPriceList (component_id, order_id, component_name, cost, description )
+VALUES
+(1,1, 'Bộ ốc vít', 50000, 'thay mới'),
+(2,1, 'Lõi đồng', 35000, 'bị cháy, thay tạm lõi cũ'),
+(3,1, 'Phích cắm', 5000, 'thay mới');
+
+--
+-- Metadata
+--
+USE `phpmyadmin`;
+
+--
+-- Metadata for table agreement
+--
+
+--
+-- Metadata for table feedback
+--
+
+--
+-- Metadata for table make
+--
+
+--
+-- Metadata for table receive
+--
+
+--
+-- Metadata for table repair_type
+--
+
+--
+-- Metadata for table service_order
+--
+
+--
+-- Metadata for table user_customer
+--
+
+--
+-- Metadata for table user_provider
+--
+
+--
+-- Metadata for database homefix
+--
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
